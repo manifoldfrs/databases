037ed3967bc2acdbe2380d9aef9604052e04d525
Faris Habib <faris.habib.7@gmail.com>
2018-10-27 20:24:52 -0700

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,13 +2,26 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE usernames (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE roomnames (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  roomname VARCHAR(255)
+);
 
-
+CREATE TABLE messages (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  message text,
+  created_at DATETIME,
+  updated_at DATETIME,
+  username_id int,
+  roomname_id int,
+  FOREIGN KEY (username_id) REFERENCES usernames(id),
+  FOREIGN KEY (roomname_id) REFERENCES roomnames(id)
+);
 
 
 /*  Execute this file from the command line by typing:
